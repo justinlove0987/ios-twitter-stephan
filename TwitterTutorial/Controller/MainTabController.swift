870398c7aec4cac9d3f89fc57c7516112c6eca1b
Justin <justin.tseng@kingwaytek.com>
2022-07-04 16:49:19 +0800

    first commit

---
+++ TwitterTutorial/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  TwitterTutorial
+//
+//  Created by justin on 2022/7/4.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
